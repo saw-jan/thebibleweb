172d34b833642a8ab7b862aea5155ff3b030e1e4
Kinsae <saw.jan.grg.3e@gmail.com>
2020-06-03 16:48:25 +0545

    Change favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,57 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        historyApiFallback: true,
-        port: 3000,
-        hot: true,
-        open: true
-    },
-    entry: path.join(__dirname, 'src', 'index.js'),
-    output: {
-        filename: './main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node-modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                    use: [
-                        {
-                            loader: 'html-loader',
-                            options: { minimize: true }
-                        }
-                    ]
-            },
-            {
-                test: /\.(ico|png|jpeg|webp|heic|heif|avif|svg|jpg|gif|eot|ttf|woff|woff2)$/,
-                use: {
-                    loader: 'file-loader'
-                }
-            },
-            {
-                test:/\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: path.join(__dirname, 'src', 'index.html'),
-            filename: './index.html',
-            favicon: path.join(__dirname, 'src', 'favicon.ico')
-        }),
-        /*
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    filename: "./main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(ico|png|jpeg|webp|heic|heif|avif|svg|jpg|gif|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+      filename: "./index.html",
+      favicon: path.join(__dirname, "src", "favico.ico"),
+    }),
+    /*
         // Used for copying files to dist folder
         new CopyPlugin({
             patterns: [
@@ -63,5 +60,5 @@ module.exports = {
             ]
         }) 
         */
-    ]
-}
+  ],
+};
